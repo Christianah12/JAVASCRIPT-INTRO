536c1ed6c2789e6efa19c16cd4ac1776db113aa1
Daniel Precious <chomzybaby39@gmail.com>
2025-09-27 12:48:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+// SWITCH STATEMENT
+// let day = "7"
+// switch (day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+
+//         case 2:
+//             console.log("Tuesday");
+//             break;
+
+//             case 3: 
+//             console.log("Wednesday");
+//             break;
+
+//             case 4:
+//                 console.log("Thursday");
+//                 break;
+
+//                 case 5:
+//                     console.log("Friday");
+//                     break;
+
+//                     case 6:
+//                         console.log("Saturday");
+//                         break;
+
+//                         case 7:
+//                             console.log("Sunday");
+//                             break;
+
+//                             default:
+//                                 console. log("Invalid day")
+// }
+
+// const grade ="F"
+// switch (grade){
+//     case "A":
+//         console.log("Excellent, wonderful job 👍");
+//         break;
+
+//         case "B":
+//             console.log("Well done!, keep up the good work 👍 ");
+//             break;
+
+//             case "C":
+//                 console.log("Good, you can do better next time");
+//                 break;
+
+//             case "D":
+//                 console.log("Keep trying, don't give up");
+//                 break;
+
+//                 case "F":
+//                     console.log("Failed! Study harder next time");
+//                     break;
+
+//                     default:
+//                         console.log("Invalid  grade entered ❌");
+// }
+
+// Assignment 2A
+// let x = 10 is a STATEMENT.
+// 3= 4*2 it is not a statement and it's not an expression either.
+// if (x=5) is a statement
+// "hello" + "world" is an expression.
+
+// // 2B STATEMENT
+// let age =24;
+// if (age > 18) {
+//     console.log("Adult");
+// }
+// // EXPRESSION
+// 4+2
+// Math.max(25,30)
+
+// TERNARY OPERATOR
+// if (temperature > 30){
+//     message = "it's hot outside";
+// } else{
+//     message = "it's cool outside!";
+// }
+
+// let temperature =30;
+// temperature = temperature > = 30 ?
+// console.log(it's hot outside);
+
+// // 3B
+// let number = 16;
+// let answer = number % 2 === 0?  "Even" : "Odd"
+// console.log(answer);
+
+// // 3C
+// let age = 27;
+// canVote = age >= 18 ? "Yes, can vote..." : "can not vote" ;
+// console.log(canVote);
+// let monthName  = prompt("Please Enter a month between 1 and 12")
+// let monthName = 3
+//   monthNumber === 1 ? "January"
+//  : monthNumber === 2 ? "Febuary"
+//  : monthNumber === 3 ? "March"
+//  : monthNumber === 4 ? "April"
+//  : monthNumber === 5 ? "May"
+//  : monthNumber === 6 ? "June"
+//  : monthNumber === 7 ? "July"
+//  : monthNumber === 8 ? "August"
+//  : monthNumber === 9 ? "September"
+//  : monthNumber === 10 ? "October"
+//  : monthNumber === 11 ? "November"
+//  : monthNumber === 12 ? "December"
+//     :    "Invalid input: Please Enter a Month between 1 and 12";
+// console.log(monthName);
+
+// 4B
+// let season = prompt("Enter Season");
+// switch (Season) {
+// case "A":
+//     alert("Summer")
+//     break;
+
+//     case "B":
+//         alert("Winter")
+//         break;
+
+//         case "C":
+//                 alert("Autumn")
+//                 break;
+
+//                 case "D":
+//                 alert ("Spring")
+//                 break;
+
+//                 default:
+//                     alert("Invalid Season Entered")
+
+// }
+
+// 4C
+// let month = prompt ("Enter month")
+// let month = 1
+// let monthequality = month % 2 === 0 ? "Even ": "Odd";
+// console.log (monthequality);
